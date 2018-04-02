024edb0f62c9f8f1ed2653a4cbd3f1726d1cb82f
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2018-04-02 14:00:02 -0300

    v0.18.04.02

--- application/controllers/Oraculo.php
+++ application/controllers/Oraculo.php
@@ -28,8 +28,31 @@ class Oraculo extends CI_controller {
 		$this -> load -> view('header/footer');
 	}
 
+	function vocabulario() {
+		$this -> load -> model("tools");
+		$tela = '';
+		$this -> cab();
+		$this -> load -> view('oraculo/search');
+
+		$cp = array();
+		$form = new form;
+		array_push($cp, array('$H8', '', '', false, false));
+		array_push($cp, array('$T80:8', '', 'Termos', true, true));
+		array_push($cp, array('$B8', '', 'Analisar >>>', false, false));
+		$tela = $form -> editar($cp, '');
+
+		if ($form -> saved > 0) {
+			$t = get("dd1");
+			$tela = '<pre>'.$this -> tools -> terms($t).'</pre>';
+		}
+		$data['content'] = $tela;
+		$this -> load -> view('content', $data);
+		$this -> foot();
+
+	}
+
 	function index() {
-		$this->load->model("tools");
+		$this -> load -> model("tools");
 		$tela = '';
 		$this -> cab();
 		$this -> load -> view('oraculo/search');
@@ -37,32 +60,86 @@ class Oraculo extends CI_controller {
 		$cmd = get("dd1");
 		$cmd = troca($cmd, ' ', ';');
 		$cmd = splitx(';', $cmd);
+		//PRINT_R($cmd);
 		if (isset($cmd[0])) {
-			switch ($cmd[0]) {
+			switch (trim($cmd[0])) {
+				case 'vocabulario' :
+					redirect(base_url('index.php/oraculo/vocabulario'));
+					break;
+				case 'fromto' :
+					$tela = '<h3>De/Para</h3>';
+					for ($r = 0; $r < 100; $r++) {
+						if (isset($_SESSION['file_' . $r])) {
+							$arq = $_SESSION['file_' . $r];
+							$tela .= '<hr>';
+							$tela .= $this -> tools -> from_to($arq);
+						}
+					}
+					break;
+				case 'open' :
+					$tela = '<h3>Open</h3>';
+					for ($r = 0; $r < 100; $r++) {
+						if (isset($_SESSION['file_' . $r])) {
+							$arq = $_SESSION['file_' . $r];
+							$tela .= '<hr>';
+							$tela .= $this -> tools -> open($arq);
+						}
+					}
+					break;
+				case 'use' :
+					$idf = get("q");
+					$arq = $this -> tools -> arquivo('_tmp/repo/', get("q"), $idf);
+					if (isset($_SESSION['file'])) {
+						$if = round($_SESSION['file']);
+						$if++;
+					} else {
+						$if = 0;
+					}
+					$_SESSION['file'] = $if;
+					$_SESSION['file_' . $if] = $arq;
+					redirect(base_url('index.php/oraculo?dd1=open'));
+					break;
+				case 'file' :
+					$idf = get("q");
+					$arq = $this -> tools -> arquivo('_tmp/repo/', get("q"), $idf);
+					$tela = '<h3>ARQUIVO: ' . get("q") . '</h3>';
+					$tela .= $this -> tools -> arquivo('_tmp/repo/', get("q"));
+					$ext = $this -> tools -> file_extension($arq);
+					$tela .= '[' . $ext . ']';
+					$tela .= '<hr>';
+					switch($ext) {
+						case 'csv' :
+							$tela .= '<a href="' . base_url('index.php/oraculo/?dd1=use&q=' . $idf) . '" class="btn btn-default">Use</a>';
+							//$tela .= '<a href="'.base_url('index.php/oraculo/?dd1=file_open&q='.$idf).'" class="btn btn-default">Open</a>';
+							break;
+					}
+					break;
+				case 'dir' :
+					$tela = '<h3>DIRETÓRIO: ' . get("d") . '</h3>';
+					$tela .= $this -> tools -> diretorio('_tmp/repo/');
+					break;
 				case 'inport' :
 					$tela = '<h3>Inport</h3>';
-					if (isset($cmd[1]))
-						{
-							switch($cmd[1])
-								{
-								case 'csv':
-									$tela .= $this->tools->file_upload();
-									$tela .= $this->tools->file_upload_save_to_temp();
-									break;
-								default:
-									$tela .= '
+					if (isset($cmd[1])) {
+						switch($cmd[1]) {
+							case 'csv' :
+								$tela .= $this -> tools -> file_upload();
+								$tela .= $this -> tools -> file_upload_save_to_temp();
+								break;
+							default :
+								$tela .= '
 										<div class="alert alert-danger" role="alert">
-										  Formato "<b>"'.$cmd[1].'</b>" desconhecido de importação
+										  Formato "<b>"' . $cmd[1] . '</b>" desconhecido de importação
 										</div>';
-									break;
-								}
-						} else {
-							$tela .= '
+								break;
+						}
+					} else {
+						$tela .= '
 							<div class="alert alert-danger" role="alert">
 							  tipo de importação não informada
 							</div>
 							';
-						}
+					}
 					break;
 			}
 		}
--- application/models/Tools.php
+++ application/models/Tools.php
@@ -12,34 +12,145 @@ class tools extends CI_model {
 				</form>';
 		return ($sx);
 	}
-	
-	function checadir($d)
-		{
-			if (!is_dir($d))
-				{
-					mkdir($d);
-				} else {
-					echo "OK ".$d.' - ';
-				}
-			return(1);
+
+	function checadir($d) {
+		if (!is_dir($d)) {
+			//echo '<br>===criando==>'.$d;
+			mkdir($d);
+		} else {
+			//echo "OK ".$d.' - ';
 		}
+		return (1);
+	}
+
+	function open($f) {
+		$sx = '';
+		$ext = $this -> file_extension($f);
+		switch ($ext) {
+			case 'csv' :
+				$sx = $this -> tools -> open_csv($f);
+				break;
+		}
+		return ($sx);
+	}
+
+	function terms($t) {
+		$t = troca($t, chr(13), ';');
+		$t = troca($t, chr(10), '');
+		$lt = splitx(';', $t);
+		$au = array();
+		$aus = array();
+		for ($r = 0; $r < count($lt); $r++) {
+			$an = $lt[$r];
+			if (strlen($an) > 0) {
+				$an = nbr_autor($an, 7);
+				if (!isset($au[$an])) {
+					$au[$an] = 1;
+					array_push($aus,$an);
+				}
+			}
+		}
+		$sx = '';
+		sort($aus);
+		for ($r=0;$r < count($aus);$r++)
+			{
+				$sx .= $aus[$r].cr();
+			}
+		return($sx);
+	}
+
+	function from_to($f) {
+		$path = '';
+		$dir = $path . '_tmp/repo/';
+		$f = $dir . $f;
+
+		if (file_exists($f)) {
+			$sx = 'OK';
+			$myfile = fopen($f, "r") or die("Unable to open file!");
+			$sx = fread($myfile, filesize($f));
+			fclose($myfile);
+		}
+	}
+
+	function open_csv($f) {
+		$path = '';
+		$dir = $path . '_tmp/repo/';
+		$f = $dir . $f;
+		if (file_exists($f)) {
+			$sx = 'OK';
+			$myfile = fopen($f, "r") or die("Unable to open file!");
+			$sx = fread($myfile, filesize($f));
+			fclose($myfile);
+			$sx = utf8_encode($sx);
+			$sx = troca($sx, ';;', ';-;');
+
+			$sx = troca($sx, chr(13), '<tr><td>');
+			$sx = troca($sx, chr(10), '');
+			$sx = troca($sx, ';', '<td>');
+
+			$sx = '<table border=1 cellpadding=4 cellspacing=2>' . $sx . '</table>';
+		} else {
+			$sx = 'File not found';
+		}
+		return ($sx);
+	}
+
+	function file_extension($f) {
+		while (strpos($f, '.')) {
+			$f = substr($f, strpos($f, '.') + 1, strlen($f));
+		}
+		return ($f);
+	}
+
+	function arquivo($d, $fl) {
+		$root = scandir($d);
+		$sx = '';
+		$f = 0;
+		foreach ($root as $value) {
+			if ($fl == $f) { $fn = $value;
+			}
+			if (is_dir($value)) {
+				$sx .= '<tt>&lt;dir&gt; ' . $value . '</tt><br>';
+			} else {
+				$sx .= '<tt><a href="' . base_url('index.php/oraculo/?dd1=file') . '&q=' . $f . '">' . $value . '</a></tt><br>';
+			}
+			$f++;
+		}
+		return ($fn);
+	}
+
+	function diretorio($d) {
+		$root = scandir($d);
+		$sx = '';
+		$f = 0;
+		foreach ($root as $value) {
+			if (is_dir($value)) {
+				$sx .= '<tt>&lt;dir&gt; ' . $value . '</tt><br>';
+			} else {
+				$sx .= '<tt><a href="' . base_url('index.php/oraculo/?dd1=file') . '&q=' . $f . '">' . $value . '</a></tt><br>';
+			}
+			$f++;
+		}
+		return ($sx);
+	}
 
 	function file_upload_save_to_temp() {
 		$sx = '';
 		if (isset($_FILES["fileToUpload"]["tmp_name"])) {
-			$target_file = $_FILES["fileToUpload"]["tmp_name"]; 
-			$FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-			
-			$dir = '_tmp';
-			$this->checadir($dir);
-			$dir = '_tmp/repo/'.date("y");
-			$this->checadir($dir);			
-			$dir = '_tmp/repo/'.date("y");
-			$this->checadir($dir);
-			$dir = '_tmp/repo/'.date("y").'/'.date("m");
-			$this->checadir($dir);
-			$target_file = $dir .= '/'.'file_001.'.$FileType;
-			
+			$target_file = $_FILES["fileToUpload"]["tmp_name"];
+			$FileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+			$path = '';
+			$dir = $path . '_tmp';
+			$this -> checadir($dir);
+			$dir = $path . '_tmp/repo/';
+			$this -> checadir($dir);
+			//$dir = $path . '_tmp/repo/' . date("Y");
+			//$this -> checadir($dir);
+			//$dir = $path . '_tmp/repo/' . date("Y") . '/' . date("m");
+			//$this -> checadir($dir);
+			$target_file = $dir .= '/' . 'file_001.' . $FileType;
+
 			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 				$sx .= "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
 			} else {
