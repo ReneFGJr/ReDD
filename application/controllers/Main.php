dca317d8be60def914ef0f1b01d95a7185d206f1
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2018-12-13 08:10:36 -0200

    v0.18.12.13

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,45 +1,52 @@
 <?php
-class Main extends CI_controller
-	{
-	function __construct() {
-		parent::__construct();
+class Main extends CI_controller {
+    function __construct() {
+        parent::__construct();
 
-		$this -> lang -> load("login", "portuguese");
-		//$this -> lang -> load("skos", "portuguese");
-		//$this -> load -> library('form_validation');
-		$this -> load -> database();
-		$this -> load -> helper('form');
-		$this -> load -> helper('form_sisdoc');
-		$this -> load -> helper('url');
-		$this -> load -> library('session');
-		date_default_timezone_set('America/Sao_Paulo');
-		/* Security */
-		//		$this -> security();
-	}		
-	public function cab($navbar=1)
-		{
-			$data['title'] = ':: ReDD - Login ::';
-			$this->load->view('header/header',$data);
-			if ($navbar==1)
-				{
-					$this->load->view('header/navbar',null);
-				}
-		}
-	public function index()
-		{
-			$this->cab();
-			
-			$tela = '<a href="'.base_url('index.php/oraculo').'">oráculo</a>';
-			$data['content'] = $tela;
-			$this->load->view('content',$data);
-		}	
-	public function researchers()
-		{
-			$this->load->model('researchers');
-			$this->cab();
-			
-			$data['content'] = $this->researchers->row();
-			$this->load->view('content',$data);
-		}
-	}
+        $this -> lang -> load("login", "portuguese");
+        //$this -> lang -> load("skos", "portuguese");
+        //$this -> load -> library('form_validation');
+        $this -> load -> database();
+        $this -> load -> helper('form');
+        $this -> load -> helper('form_sisdoc');
+        $this -> load -> helper('url');
+        $this -> load -> library('session');
+        date_default_timezone_set('America/Sao_Paulo');
+        /* Security */
+        //		$this -> security();
+    }
+
+    public function cab($navbar = 1) {
+        $data['title'] = ':: ReDD - Login ::';
+        $this -> load -> view('header/header', $data);
+        if ($navbar == 1) {
+            $this -> load -> view('header/navbar', null);
+        }
+    }
+
+    public function ata($id = 0) {
+        $this -> load -> model("office");
+        $this -> cab();
+        $tela = $this -> office -> ata($id);
+        $data['content'] = $tela;
+        $this -> load -> view('content', $data);
+    }
+
+    public function index() {
+        $this -> cab();
+
+        $tela = '<a href="' . base_url('index.php/oraculo') . '">oráculo</a>';
+        $data['content'] = $tela;
+        $this -> load -> view('content', $data);
+    }
+
+    public function researchers() {
+        $this -> load -> model('researchers');
+        $this -> cab();
+
+        $data['content'] = $this -> researchers -> row();
+        $this -> load -> view('content', $data);
+    }
+
+}
 ?>
---
+++ application/models/Office.php
@@ -0,0 +1,17 @@
+<?php
+class office extends CI_model
+    {
+        function ata_edit($id=0)
+            {
+                
+            } 
+        function ata($id=0)
+            {
+                $sx = '';
+                $sx .= '<div class="code gutter">'.cr();
+                $sx .= 'Hello World!';
+                $sx .= '</div>'.cr();
+                return($sx);
+            }  
+    }
+?>
